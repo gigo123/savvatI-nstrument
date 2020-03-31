303b49d15de3f2cf0e34c98cd63bb371d96814ae
Taras Hopka <gigo123black@gmail.com>
2020-03-31 12:43:04 +0300

    add model for instrument

---
+++ src/main/java/models/Instrument.java
@@ -0,0 +1,44 @@
+package models;
+
+public class Instrument {
+	private int id;
+	private String name;
+	private String comment;
+	private String measure;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getMeasure() {
+		return measure;
+	}
+	public void setMeasure(String measure) {
+		this.measure = measure;
+	}
+	
+	public Instrument(String name, String measure) {
+		super();
+		this.name = name;
+		this.measure = measure;
+	}
+	public Instrument(String name, String measure, String comment) {
+		this(name, measure);
+		this.comment = comment;
+	}
+	public Instrument() {};
+}
