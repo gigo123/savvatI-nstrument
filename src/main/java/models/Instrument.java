ecaa111c81b862eab400619744fd1d1886cb90a6
Taras Hopka <gigo123black@gmail.com>
2020-03-31 15:10:24 +0300

    change id to long

--- src/main/java/models/Instrument.java
+++ src/main/java/models/Instrument.java
@@ -1,15 +1,15 @@
 package models;
 
 public class Instrument {
-	private int id;
+	private long id;
 	private String name;
 	private String comment;
 	private String measure;
 	
-	public int getId() {
+	public long getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 	public String getName() {
