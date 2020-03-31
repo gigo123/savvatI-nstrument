0742abfd088bf12fb5d5e5229880e1ee373e6878
Taras Hopka <gigo123black@gmail.com>
2020-03-31 13:01:28 +0300

    model for outDoc

---
+++ src/main/java/models/OutDoc.java
@@ -0,0 +1,56 @@
+package models;
+
+public class OutDoc {
+	private int id;
+	private Location outLocation;
+	private Box outBox;
+	private String date;
+	private Instrument instrument;
+	private float amount;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Location getOutLocation() {
+		return outLocation;
+	}
+	public void setOutLocation(Location outLocation) {
+		this.outLocation = outLocation;
+	}
+	public Box getOutBox() {
+		return outBox;
+	}
+	public void setOutBox(Box outBox) {
+		this.outBox = outBox;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public Instrument getInstrument() {
+		return instrument;
+	}
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public OutDoc(Location outLocation, Box outBox, String date, Instrument instrument, float amount) {
+		super();
+		this.outLocation = outLocation;
+		this.outBox = outBox;
+		this.date = date;
+		this.instrument = instrument;
+		this.amount = amount;
+	}
+	public OutDoc() {
+	}
+}
