d514d46217113de1dcc1fddf0a6a15f76e412b1f
Taras Hopka <gigo123black@gmail.com>
2020-03-31 09:15:56 +0300

    new project

---
+++ src/main/java/savvats/App.java
@@ -0,0 +1,13 @@
+package savvats;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
