86835b91a5f7ced0ebe91604cbc6c7feb5c9b7dc
Taras Hopka <gigo123black@gmail.com>
2020-04-01 12:45:03 +0300

    change list from instrument to box

--- src/main/java/dao/InstrumentDAO.java
+++ src/main/java/dao/InstrumentDAO.java
@@ -9,10 +9,10 @@ import models.Location;
 public interface InstrumentDAO {
 	public Instrument getInstrumentByID(long id);
 	public Instrument getInstrumentByName(String name);
-	public List<Instrument> getInstrumentByNameL(String name);
-	public List<Instrument> getInstrumentByBox(long idB,int idL);
-	public List<Instrument> getInstrumentByLocation(Location loacation);
-	public List<Instrument> getAllInstrument();
+	public List<Box> getInstrumentByNameL(String name);
+	public List<Box> getInstrumentByBox(long idB,int idL);
+	public List<Box> getInstrumentByLocation(Location loacation);
+	public List<Box> getAllInstrument();
 	public boolean createInstrument(String name, String measure, String comment);
 	public boolean createInstrument(String name, String measure);
 	public boolean deleteInstrument(int id);
