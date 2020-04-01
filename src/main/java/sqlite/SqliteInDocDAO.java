6dd47ab2d20db72d47278f98746d050ca006f168
Taras Hopka <gigo123black@gmail.com>
2020-04-01 12:46:07 +0300

    imlements wrong inteface correct

--- src/main/java/sqlite/SqliteInDocDAO.java
+++ src/main/java/sqlite/SqliteInDocDAO.java
@@ -2,64 +2,49 @@ package sqlite;
 
 import java.util.List;
 
-import dao.InstrumentDAO;
+import dao.InDocDAO;
+import models.Box;
+import models.InDoc;
 import models.Instrument;
 import models.Location;
 
-public class SqliteInDocDAO implements InstrumentDAO{
+public class SqliteInDocDAO implements InDocDAO{
 
 	@Override
-	public Instrument getInstrumentByID(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Instrument getInstrumentByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Instrument> getInstrumentByNameL(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Instrument> getInstrumentByBox(long idB, int idL) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Instrument> getInstrumentByLocation(Location loacation) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Instrument> getAllInstrument() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean createInstrument(String name, String measure, String comment) {
+	public boolean createInDoc(Location inLocation, Box inBox, String date, Instrument instrument, float amount) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean createInstrument(String name, String measure) {
+	public InDoc getInDocById(int id) {
 		// TODO Auto-generated method stub
-		return false;
+		return null;
 	}
 
 	@Override
-	public boolean deleteInstrument(int id) {
+	public List<InDoc> getInDocByDate(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<InDoc> getInDocByInstrum(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<InDoc> getInDocByBox(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteInDoc(int id) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	
 }
