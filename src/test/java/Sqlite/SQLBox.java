93200f0804aab34606cc4e71a7679530fb9d54f2
Taras Hopka <gigo123black@gmail.com>
2020-04-03 09:03:35 +0300

    new test class for sqlBox

---
+++ src/test/java/Sqlite/SQLBox.java
@@ -0,0 +1,75 @@
+package Sqlite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+
+import org.junit.jupiter.api.Test;
+
+import models.Box;
+import models.Instrument;
+import sqlite.SQLConectionHolder;
+import sqlite.SqliteBoxDAO;
+import sqlite.SqliteInstrumentDAO;
+import sqlite.SqliteLocationDAO;
+
+class SQLBox {
+	SqliteBoxDAO boxDAO;
+	SqliteLocationDAO locationDAO;
+	SqliteInstrumentDAO instrumentDAO;
+	
+	void initConnection() {
+		SQLConectionHolder conectHolder = new SQLConectionHolder();
+		conectHolder.setConnString("jdbc:mysql://localhost/instrument1?user=root&password=");
+		boxDAO = new SqliteBoxDAO();
+		boxDAO.setConectionHolder(conectHolder);
+		locationDAO = new SqliteLocationDAO();
+		locationDAO.setConectionHolder(conectHolder);
+		instrumentDAO = new SqliteInstrumentDAO();
+		instrumentDAO.setConectionHolder(conectHolder);
+	}
+
+	@Test
+	void setConnection() {
+		initConnection();
+		SQLConectionHolder conectHolder2  = boxDAO.getConectionHolder();
+		Connection conn =conectHolder2.getConnection();
+		assertTrue(conn!=null,"connetion must not be null");
+	}
+	@Test
+	void createBox() {
+		SqliteBoxDAO  boxDAOc = new SqliteBoxDAO();
+		Box box = new Box(1, locationDAO.getLocById(1), instrumentDAO.getInstrumentByID(1), 1);
+		boxDAOc.createBox(box);
+		boolean error = boxDAOc.hasError();
+		assertTrue(error,"must be error");
+		initConnection();
+		boxDAO.createBox(box);
+		error = boxDAO.hasError();
+		assertTrue(!error,"must be ok");
+	}
+	@Test
+	void getBoxById() {
+		SqliteBoxDAO  boxDAOc = new SqliteBoxDAO();
+		boxDAOc.getBoxByID(1);
+		boolean error = boxDAOc.hasError();
+		assertTrue(error,"must be error");
+		initConnection();
+		 boxDAO.getBoxByID(1);
+		error = boxDAO.hasError();
+		assertTrue(!error,"must be ok");
+	}
+	@Test
+	void getBoxByNumber() {
+		SqliteBoxDAO  boxDAOc = new SqliteBoxDAO();
+		boxDAOc.getBoxByNumber(1, 1);
+		boolean error = boxDAOc.hasError();
+		assertTrue(error,"must be error");
+		initConnection();
+		Box box  = boxDAO.getBoxByNumber(1, 1);
+		error = boxDAO.hasError();
+		assertTrue(!error,"must be ok");
+		assertTrue(box.getNumber()==1,"must be test1");
+	}
+
+}
