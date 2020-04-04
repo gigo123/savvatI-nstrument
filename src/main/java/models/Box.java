08e25265d889143ce823e7a9a56fc696edf52a4e
Taras Hopka <gigo123black@gmail.com>
2020-04-04 09:25:33 +0300

    edit model
    split box to box and storage

--- src/main/java/models/Box.java
+++ src/main/java/models/Box.java
@@ -5,8 +5,6 @@ public class Box {
 	private long id;
 	private int number;
 	private Location location;
-	private Instrument instrument;
-	private float amount;
 	
 	public long getId() {
 		return id;
@@ -26,25 +24,7 @@ public class Box {
 	public void setLocation(Location location) {
 		this.location = location;
 	}
-	public Instrument getInstruments() {
-		return instrument;
-	}
-	public void setInstruments(Instrument instruments) {
-		this.instrument = instruments;
-	}
-	public float getInstrumentsNumbers() {
-		return amount;
-	}
-	public void setInstrumentsNumbers(float instrumentsNumbers) {
-		amount = instrumentsNumbers;
-	}
-	public Box(int number, Location location, Instrument instruments, float instrumentsNumbers) {
-		super();
-		this.number = number;
-		this.location = location;
-		this.instrument = instruments;
-		this.amount= instrumentsNumbers;
-	}
+	
 	public Box(int number, Location location) {
 		super();
 		this.number = number;
