2e7cd379243f7361aae0d40cc3a84c6c5f8ab461
Taras Hopka <gigo123black@gmail.com>
2020-04-04 11:33:28 +0300

    new class extends Box for web page

---
+++ src/main/java/savvats/BoxListLocation.java
@@ -0,0 +1,17 @@
+package savvats;
+
+import models.Box;
+
+public class BoxListLocation extends Box{
+	private String locationWB;
+
+	public String getLocationWB() {
+		return locationWB;
+	}
+
+	public void setLocationWB(String locationWB) {
+		this.locationWB = locationWB;
+	}
+	
+
+}
