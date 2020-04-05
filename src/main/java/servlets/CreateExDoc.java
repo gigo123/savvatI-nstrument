b5234ef87bb637236da4ca32162525286288688c
Taras Hopka <gigo123black@gmail.com>
2020-04-05 16:49:43 +0300

    create method for create Map of visible item
    and work  with get method

--- src/main/java/servlets/CreateExDoc.java
+++ src/main/java/servlets/CreateExDoc.java
@@ -1,5 +1,9 @@
 package servlets;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -9,23 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import models.ExDoc;
+import dao.LocationDAO;
+import models.Location;
+import savvats.ExDocWEB;
 
 @Controller
 @RequestMapping("/createExDoc")
 public class CreateExDoc {
 	private boolean error = false;
 	private StringBuilder errorText;
-
+	private Map<Integer, ExDocWEB> docMap= new HashMap<Integer, ExDocWEB>();
+	private int docCount;
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getExDocCF() {
-		ModelAndView model = new ModelAndView("CreateExDoc", "command", new ExDoc());
+		docMap.put(docCount, new ExDocWEB());
+		ModelAndView model = new ModelAndView("CreateExDoc", "command", docMap);
 		return model;
+		
 	}
 
 	@SuppressWarnings("resource")
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView postBoxCF(@ModelAttribute("SpringWeb") ExDoc doc, ModelMap model) {
+	public ModelAndView postBoxCF(@ModelAttribute("SpringWeb")  Map<Integer, ExDocWEB> rdocMap, ModelMap model) {
+		docMap = rdocMap;
 		error = false;
 		errorText = new StringBuilder("<ul>");
 		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
@@ -40,5 +50,19 @@ public class CreateExDoc {
 			model.addObject("errorText", errorText.toString());
 		}
 		return model;
+	}
+	
+	private ModelAndView createMaps(ModelAndView model) {
+		
+		 Map<Long, String> LociationMap= new HashMap<Long, String>();
+		 ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+			LocationDAO locDAO = (LocationDAO) context.getBean("LoctionDAO");
+			List<Location> locList = locDAO.getAllLocatin();
+			for (Location location : locList) {
+				LociationMap.put(location.getId(), location.getName());
+			}
+			
+		return model;
+		
 	}
 }
