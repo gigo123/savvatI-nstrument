70c62e4c7e1b3139cb5c7aa3142cdb224d2aeac2
Taras Hopka <gigo123black@gmail.com>
2020-04-05 14:15:14 +0300

    add instrument Work method to ControllersCheckWrite

--- src/main/java/savvats/ControllersCheckWrite.java
+++ src/main/java/savvats/ControllersCheckWrite.java
@@ -3,45 +3,81 @@ package savvats;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import dao.InstrumentDAO;
 import dao.LocationDAO;
+import models.Instrument;
 import models.Location;
 
 public class ControllersCheckWrite {
 
 	@SuppressWarnings("resource")
-	public  static String  addLocationWork(Location location) {
-		StringBuilder errorText =  new StringBuilder("<ul>");
+	public static String addLocationWork(Location location) {
+		StringBuilder errorText = new StringBuilder("<ul>");
 		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 		LocationDAO locDAO = (LocationDAO) context.getBean("LocationDAO");
 		Location loc = locDAO.getLocByName(location.getName());
-		boolean error=false;
+		boolean error = false;
 		if (location.getName().length() < 4) {
-			error=true;
+			error = true;
 			errorText.append("<li> короткое имя </li>");
 		}
 		if (locDAO.hasError()) {
-			error=true;
+			error = true;
 			errorText.append("<li> ошыбка бази данних </li>");
 		} else {
 			if (loc != null) {
-				error=true;
+				error = true;
 				errorText.append("<li> место хранения существует </li>");
 			}
 		}
-		if(!error) {
-		if (!locDAO.createLocation(location)) {
-			error = true;
-			errorText.append("<li>ошыбка бази данних </li>");
-		}
+		if (!error) {
+			if (!locDAO.createLocation(location)) {
+				error = true;
+				errorText.append("<li>ошыбка бази данних </li>");
+			}
 		}
 		errorText.append("</ul>");
 		String errString = errorText.toString();
-		if(errString.equals("<ul></ul>")) {
+		if (errString.equals("<ul></ul>")) {
 			return "место хранения успешно создано";
-		}
-		else {
+		} else {
 			return errString;
 		}
-		
+
+	}
+
+	@SuppressWarnings("resource")
+	public static String addInstrumentWork(Instrument ininstr) {
+		StringBuilder errorText = new StringBuilder("<ul>");
+		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+		InstrumentDAO instDAO = (InstrumentDAO) context.getBean("InstrumentDAO");
+		boolean error = false;
+		if (ininstr.getName().length() < 4) {
+			error = true;
+			errorText.append("<li> короткое имя </li>");
+		}
+		Instrument instrum = instDAO.getInstrumentByName(ininstr.getName());
+		if (instDAO.hasError()) {
+			error = true;
+			errorText.append("<li> ошыбка бази данних </li>");
+		} else {
+			if (instrum != null) {
+				error = true;
+				errorText.append("<li> место хранения существует </li>");
+			}
+		}
+		if (!error) {
+			if (!instDAO.createInstrument(instrum)) {
+				error = true;
+				errorText.append("<li>ошыбка бази данних </li>");
+			}
+		}
+		errorText.append("</ul>");
+		String errString = errorText.toString();
+		if (errString.equals("<ul></ul>")) {
+			return "место хранения успешно создано";
+		} else {
+			return errString;
+		}
 	}
 }
--- src/main/java/servlets/AddInstrument.java
+++ src/main/java/servlets/AddInstrument.java
@@ -1,7 +1,5 @@
 package servlets;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,58 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import dao.InstrumentDAO;
 import models.Instrument;
+import savvats.ControllersCheckWrite;
 
 @Controller
 @RequestMapping("/addinstument")
 public class AddInstrument {
-	private boolean error = false;
-	private StringBuilder errorText;
-	private InstrumentDAO instDAO;
-
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getInstrumentCF() {
 		ModelAndView model = new ModelAndView("AddInstrument", "command", new Instrument());
-		if (error) {
-			error = false;
-			model.addObject("errorText", errorText.toString());
-		}
 		return model;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView postInstrumentCF(@ModelAttribute("SpringWeb") Instrument instrument, ModelMap model) {
-		error = false;
-		errorText = new StringBuilder("<ul>");
-		@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-		instDAO = (InstrumentDAO) context.getBean("InstrumentDAO");
-		checkErrors(instrument);
-		if (!error) {
-			if (!instDAO.createInstrument(instrument)) {
-				error = true;
-				errorText.append("<li>ошыбка бази данних </li>");
-			}
-		}
-		return getInstrumentCF();
+		String message = ControllersCheckWrite.addInstrumentWork(instrument);
+		return showInfoPage(message);
 	}
 
-	private void checkErrors(Instrument inst) {
-		if (inst.getName().length() < 4) {
-			error = true;
-			errorText.append("<li> короткое имя </li>");
-		}
-		Instrument instrum = instDAO.getInstrumentByName(inst.getName());
-		if (instDAO.hasError()) {
-			error = true;
-			errorText.append("<li> ошыбка бази данних </li>");
-		} else {
-			if (instrum != null) {
-				error = true;
-				errorText.append("<li> место хранения существует </li>");
-			}
-		}
+	public ModelAndView showInfoPage(String message) {
+		ModelAndView model = new ModelAndView("OperationInfo");
+		model.addObject("errorText", message);
+		return model;
 	}
 
 }
