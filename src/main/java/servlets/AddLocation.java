b8354ee9e18c6a606f308dbb0df91ce3997f4d65
Taras Hopka <gigo123black@gmail.com>
2020-04-05 12:31:05 +0300

    add work method from ControllersCheckWrite

--- src/main/java/servlets/AddLocation.java
+++ src/main/java/servlets/AddLocation.java
@@ -1,7 +1,6 @@
 package servlets;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,15 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import dao.LocationDAO;
 import models.Location;
+import savvats.ControllersCheckWrite;
 
 @Controller
 @RequestMapping("/addlocation")
 public class AddLocation {
-	private boolean error = false;
-	private StringBuilder errorText;
-	private LocationDAO locDAO;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getLocationCF() {
@@ -27,44 +23,13 @@ public class AddLocation {
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView postLocationCF(@ModelAttribute("SpringWeb") Location location, ModelMap model) {
-		error = false;
-		errorText = new StringBuilder("<ul>");
-		@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-		locDAO = (LocationDAO) context.getBean("LocationDAO");
-		checkErrors(location);
-		if (!error) {
-			if (!locDAO.createLocation(location)) {
-				error = true;
-				errorText.append("<li>ошыбка бази данних </li>");
-			}
-		}
-		return showInfoPage();
+		String message = ControllersCheckWrite.addLocationWork(location);
+		return showInfoPage(message);
 	}
 
-	private void checkErrors(Location location) {
-		if (location.getName().length() < 4) {
-			error = true;
-			errorText.append("<li> короткое имя </li>");
-		}
-		Location loc = locDAO.getLocByName(location.getName());
-		if (locDAO.hasError()) {
-			error = true;
-			errorText.append("<li> ошыбка бази данних </li>");
-		} else {
-			if (loc != null) {
-				error = true;
-				errorText.append("<li> место хранения существует </li>");
-			}
-		}
-	}
-	
-	public ModelAndView showInfoPage() {
-		ModelAndView model = new ModelAndView();
-		if (error) {
-			error = false;
-			model.addObject("errorText", errorText.toString());
-		}
+	public ModelAndView showInfoPage(String message) {
+		ModelAndView model = new ModelAndView("OperationInfo");
+		model.addObject("errorText", message);
 		return model;
 	}
 
