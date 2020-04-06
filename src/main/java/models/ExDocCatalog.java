49ab5f03990101f25793d431dc7d5203d55576ca
Taras Hopka <gigo123black@gmail.com>
2020-04-06 09:29:01 +0300

    comlete model of ExDocCatalog

--- src/main/java/models/ExDocCatalog.java
+++ src/main/java/models/ExDocCatalog.java
@@ -7,4 +7,41 @@ public class ExDocCatalog {
 	private String numberString;
 	private int number;
 	private LocalDate date;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNumberString() {
+		return numberString;
+	}
+	public void setNumberString(String numberString) {
+		this.numberString = numberString;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public ExDocCatalog(String numberString, int number, LocalDate date) {
+		super();
+		this.numberString = numberString;
+		this.number = number;
+		this.date = date;
+	}
+	@Override
+	public String toString() {
+		return "ExDocCatalog [id=" + id + ", numberString=" + numberString + ", number=" + number + ", date=" + date
+				+ "]";
+	}
+	
+	
 }
