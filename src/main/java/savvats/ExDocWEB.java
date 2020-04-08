3143609aba7b70f15f27089b71e98055bf4cb474
Taras Hopka <gigo123black@gmail.com>
2020-04-08 18:06:23 +0300

    make box variable  int, add validators

--- src/main/java/savvats/ExDocWEB.java
+++ src/main/java/savvats/ExDocWEB.java
@@ -1,10 +1,8 @@
 package savvats;
 
-import java.time.LocalDate;
-
 import javax.validation.constraints.NotEmpty;
-
-import org.hibernate.validator.constraints.Range;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 
 public class ExDocWEB {
 	private long id;
@@ -12,14 +10,13 @@ public class ExDocWEB {
 	private String outLocation;
 	 @NotEmpty
 	private String inLocation;
-	 
-	private String outBox;
-	private String inBox;
-	 @NotEmpty
-	private LocalDate date;
+	@PositiveOrZero
+	private int outBox;
+	@PositiveOrZero
+	private int inBox;
 	 @NotEmpty
 	private String instrument;
-	 @Range(min = 0, max = 200)
+	 @Positive
 	private float amount;
 	
 	public long getId() {
@@ -40,24 +37,19 @@ public class ExDocWEB {
 	public void setInLocation(String inLocation) {
 		this.inLocation = inLocation;
 	}
-	public String getOutBox() {
+	public int getOutBox() {
 		return outBox;
 	}
-	public void setOutBox(String outBox) {
+	public void setOutBox(int outBox) {
 		this.outBox = outBox;
 	}
-	public String getInBox() {
+	public int getInBox() {
 		return inBox;
 	}
-	public void setInBox(String inBox) {
+	public void setInBox(int inBox) {
 		this.inBox = inBox;
 	}
-	public LocalDate getDate() {
-		return date;
-	}
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+	
 	public String getInstrument() {
 		return instrument;
 	}
@@ -70,14 +62,12 @@ public class ExDocWEB {
 	public void setAmount(float amount) {
 		this.amount = amount;
 	}
-	public ExDocWEB(String outLocation, String inLocation, String outBox, String inBox, LocalDate date,
-			String instrument, float amount) {
+	public ExDocWEB(String outLocation, String inLocation, int outBox, int inBox, String instrument, float amount) {
 		super();
 		this.outLocation = outLocation;
 		this.inLocation = inLocation;
 		this.outBox = outBox;
 		this.inBox = inBox;
-		this.date = date;
 		this.instrument = instrument;
 		this.amount = amount;
 	}
@@ -87,7 +77,7 @@ public class ExDocWEB {
 	@Override
 	public String toString() {
 		return "ExDocWEB [id=" + id + ", outLocation=" + outLocation + ", inLocation=" + inLocation + ", outBox="
-				+ outBox + ", inBox=" + inBox + ", date=" + date + ", instrument=" + instrument + ", amount=" + amount
+				+ outBox + ", inBox=" + inBox + ", instrument=" + instrument + ", amount=" + amount
 				+ "]";
 	}
 	
