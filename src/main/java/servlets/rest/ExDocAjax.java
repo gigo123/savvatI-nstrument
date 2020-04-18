08e9876493d49f4da2616e806d30445d3c37b0a2
Taras Hopka <gigo123black@gmail.com>
2020-04-18 14:09:12 +0300

    create new object to incoporate map of boxes

--- src/main/java/servlets/rest/ExDocAjax.java
+++ src/main/java/servlets/rest/ExDocAjax.java
@@ -1,5 +1,6 @@
 package servlets.rest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,59 +21,73 @@ import savvats.SearchById;
 import savvats.Views;
 import savvats.ajax.AjaxResponseDocBox;
 import savvats.ajax.AjaxResponseDocInstrument;
+import savvats.ajax.DocBoxList;
 
 @RestController
 @RequestMapping("/createExDoc")
 public class ExDocAjax {
 
+	@SuppressWarnings("resource")
 	@JsonView(Views.Public.class)
 	@RequestMapping("/getBoxFilter")
 	public AjaxResponseDocBox getSearchBoxResultViaAjax(@RequestBody SearchById search) {
 
 		AjaxResponseDocBox result = new AjaxResponseDocBox();
 		result.setCode("200");
-
-		//long LocId = Long.parseLong(search.getBoxId());
-
-		result.setMsg("hello");
-
-	/*	Map<Long, Integer> boxMap = new HashMap<Long, Integer>();
-		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 		BoxDAO boxDAO = (BoxDAO) context.getBean("BoxDAO");
-		List<Box> BoxList = boxDAO.getAllBoxByLocation(LocId);
-		for (Box box : BoxList) {
-			boxMap.put(box.getId(), box.getNumber());
+		// long LocId = Long.parseLong(search.getBoxId());
+		List<Integer> boxIdList = search.getBoxId();
+
+		String msg="";
+		List<DocBoxList> boxListId = new ArrayList<DocBoxList>();
+		DocBoxList boxListMap = new DocBoxList();
+		for (int boxId : boxIdList) {
+			long LocId = boxId;
+			//msg=msg + " " + LocId;
+			Map<Long, Integer> boxMap = new HashMap<Long, Integer>();
+			List<Box> BoxList = boxDAO.getAllBoxByLocation(LocId);
+			for (Box box : BoxList) {
+				System.out.println(box.getId());
+				boxMap.put(box.getId(), box.getNumber());
+			}
+			System.out.println(boxMap);
+			boxListMap.setBoxMap(boxMap);
+			boxListId.add(boxListMap);
+			System.out.println(boxListId);
+			msg= boxListId.toString();
 		}
-		result.setBoxMap(boxMap);
-		*/
+		result.setMsg(msg);
+		result.setBoxListId(boxListId);
 		return result;
 
 	}
 
-/*	@JsonView(Views.Public.class)
-	@RequestMapping("/getInstrumentFilter")
-	public AjaxResponseDocInstrument getSearchInstrumentResultViaAjax(@RequestBody SearchById search) {
-
-		AjaxResponseDocInstrument result = new AjaxResponseDocInstrument();
-		result.setCode("200");
-
-		long boxId = Long.parseLong(search.getBoxId());
-
-		result.setMsg("boxId");
-
-		Map<Long, String> instrumentMap = new HashMap<Long, String>();
-		@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-		StorageDAO storeDAO = (StorageDAO) context.getBean("StorageDAO");
-		List<Storage> storeList = storeDAO.getStorageByBox(boxId);
-
-		for (Storage storsge : storeList) {
-			instrumentMap.put(storsge.getInstrument().getId(), storsge.getInstrument().getName());
-		}
-		result.setInstrumentMap(instrumentMap);
-		return result;
-
-	}
-*/
+	/*
+	 * @JsonView(Views.Public.class)
+	 * 
+	 * @RequestMapping("/getInstrumentFilter") public AjaxResponseDocInstrument
+	 * getSearchInstrumentResultViaAjax(@RequestBody SearchById search) {
+	 * 
+	 * AjaxResponseDocInstrument result = new AjaxResponseDocInstrument();
+	 * result.setCode("200");
+	 * 
+	 * long boxId = Long.parseLong(search.getBoxId());
+	 * 
+	 * result.setMsg("boxId");
+	 * 
+	 * Map<Long, String> instrumentMap = new HashMap<Long, String>();
+	 * 
+	 * @SuppressWarnings("resource") ApplicationContext context = new
+	 * ClassPathXmlApplicationContext("application-context.xml"); StorageDAO
+	 * storeDAO = (StorageDAO) context.getBean("StorageDAO"); List<Storage>
+	 * storeList = storeDAO.getStorageByBox(boxId);
+	 * 
+	 * for (Storage storsge : storeList) {
+	 * instrumentMap.put(storsge.getInstrument().getId(),
+	 * storsge.getInstrument().getName()); } result.setInstrumentMap(instrumentMap);
+	 * return result;
+	 * 
+	 * }
+	 */
 }
