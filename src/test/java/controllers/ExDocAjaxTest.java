2b3c2cc07c8d2b82e7d3592f62488df325ee33d6
Taras Hopka <gigo123black@gmail.com>
2020-04-18 18:26:43 +0300

    new test for select instrument

--- src/test/java/controllers/ExDocAjaxTest.java
+++ src/test/java/controllers/ExDocAjaxTest.java
@@ -27,29 +27,50 @@ public class ExDocAjaxTest {
 	MockMvc mockMvc;
 
 	@Test
-	public void add() {
-		this.mockMvc =MockMvcBuilders.standaloneSetup(ExDocAjax.class).build();
-	//	this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).dispatchOptions(true).build();
+	public void setBox() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(ExDocAjax.class).build();
+		// this.mockMvc =
+		// MockMvcBuilders.webAppContextSetup(this.wac).dispatchOptions(true).build();
 		List<Integer> boxIdList = new ArrayList<Integer>();
 		boxIdList.add(1);
 		boxIdList.add(2);
 		SearchById search = new SearchById();
 		search.setBoxId(boxIdList);
-		//  создаем POST-запрос, набиваем его параметрами и выполняем
+		// создаем POST-запрос, набиваем его параметрами и выполняем
 		String searchString = "{\"boxId\":[\"1\",\"7\"]}";
 		try {
-			mockMvc.perform(MockMvcRequestBuilders.post("/createExDoc/getBoxFilter")
-			.content(searchString)
-			.contentType(MediaType.APPLICATION_JSON))
-			   .andExpect(status().isOk())
-			   .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-			   .andExpect(jsonPath("boxListId").isArray());
+			mockMvc.perform(MockMvcRequestBuilders.post("/createExDoc/getBoxFilter").content(searchString)
+					.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+					.andExpect(jsonPath("boxListId").isArray());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
 	
-}
+	
+	@Test
+	public void setInstrument() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(ExDocAjax.class).build();
+		// this.mockMvc =
+		// MockMvcBuilders.webAppContextSetup(this.wac).dispatchOptions(true).build();
+		List<Integer> boxIdList = new ArrayList<Integer>();
+		boxIdList.add(1);
+		boxIdList.add(2);
+		SearchById search = new SearchById();
+		search.setBoxId(boxIdList);
+		// создаем POST-запрос, набиваем его параметрами и выполняем
+		String searchString = "{\"boxId\":[\"13\",\"9\"]}";
+		try {
+			mockMvc.perform(MockMvcRequestBuilders.post("/createExDoc//getInstrumentFilter").content(searchString)
+					.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+					.andExpect(jsonPath("boxListId").isArray());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
