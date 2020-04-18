d13ff6aaff3f5423d934576925fb0c677789bc6f
Taras Hopka <gigo123black@gmail.com>
2020-04-18 17:27:10 +0300

    ilmplement seriazation and hashCode

--- src/main/java/savvats/ajax/DocBoxList.java
+++ src/main/java/savvats/ajax/DocBoxList.java
@@ -1,12 +1,19 @@
 package savvats.ajax;
 
+import java.io.Serializable;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
 import savvats.Views;
 
-public class DocBoxList {
+public class DocBoxList implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8547032882410410269L;
+	
+	
 	@JsonView(Views.Public.class)
 	Map<Long, Integer> BoxMap;
 
@@ -16,12 +23,37 @@ public class DocBoxList {
 	}
 
 	public void setBoxMap(Map<Long, Integer> boxMap) {
-		BoxMap = boxMap;
+		this.BoxMap = boxMap;
 	}
 
 	@Override
 	public String toString() {
 		return "DocBoxList [BoxMap=" + BoxMap + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((BoxMap == null) ? 0 : BoxMap.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DocBoxList other = (DocBoxList) obj;
+		if (BoxMap == null) {
+			if (other.BoxMap != null)
+				return false;
+		} else if (!BoxMap.equals(other.BoxMap))
+			return false;
+		return true;
 	}
 	
 }
