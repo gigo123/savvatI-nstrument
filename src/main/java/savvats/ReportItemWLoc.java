b3067a5bb811d5126a22a9a411759fc123296cbd
Taras Hopka <gigo123black@gmail.com>
2020-05-11 11:16:29 +0300

    new object to display report by instrument

---
+++ src/main/java/savvats/ReportItemWLoc.java
@@ -0,0 +1,25 @@
+package savvats;
+
+public class ReportItemWLoc  extends ReportItem{
+	private String locationName;
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+
+	public ReportItemWLoc(String name, float amount, String measure, String locationName) {
+		super(name, amount, measure);
+		this.locationName = locationName;
+	}
+
+	@Override
+	public String toString() {
+		return "ReportItemWLoc [locationName=" + locationName + "]";
+	}
+	
+
+}
